89a77ca3a0ef474915b27c39bed7f4b55cfef573
babicvlcnt <bcfrota@id.uff.br>
2022-01-22 09:20:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = "Bárbara"
+let number = 27
+let anoDeNasc = 2022 - 26
+console.log(anoDeNasc)
+console.log(`${nome} está mexendo no console.log`)
+let diaDaSemana = "Quarta-feira"
+let boolean = "falso"
+console.log(`Hoje não é ${diaDaSemana}, portanto o valor atribuído é ${boolean}`)
+//não sei como exibir no console o tipo de dado de uma variável...
